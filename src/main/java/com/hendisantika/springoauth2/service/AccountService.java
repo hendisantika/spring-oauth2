9803ab66589f28f29bb954682982d86a374a2f3a
Hendi Santika <hendisantika@gmail.com>
2018-07-05 06:37:36 +0700

    #9 - Add Account Service

---
+++ src/main/java/com/hendisantika/springoauth2/service/AccountService.java
@@ -0,0 +1,72 @@
+package com.hendisantika.springoauth2.service;
+
+import com.hendisantika.springoauth2.entity.Account;
+import com.hendisantika.springoauth2.entity.Role;
+import com.hendisantika.springoauth2.repository.AccountRepo;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-oauth2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/07/18
+ * Time: 06.33
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class AccountService implements UserDetailsService {
+
+    private static final Logger logger = LogManager.getLogger(AccountService.class);
+
+    @Autowired
+    private AccountRepo accountRepo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        Optional<Account> account = accountRepo.findByUsername(s);
+        if (account.isPresent()) {
+            return account.get();
+        } else {
+            throw new UsernameNotFoundException(String.format("Username[%s] not found", s));
+        }
+    }
+
+    public Account findAccountByUsername(String username) throws UsernameNotFoundException {
+        Optional<Account> account = accountRepo.findByUsername(username);
+        if (account.isPresent()) {
+            return account.get();
+        } else {
+            throw new UsernameNotFoundException(String.format("Username[%s] not found", username));
+        }
+    }
+
+    public Account registerUser(Account account) {
+        account.setPassword(passwordEncoder.encode(account.getPassword()));
+        account.grantAuthority(Role.ROLE_USER);
+        return accountRepo.save(account);
+    }
+
+    @Transactional // To successfully remove the date @Transactional annotation must be added
+    public boolean removeAuthenticatedAccount() throws UsernameNotFoundException {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Account acct = findAccountByUsername(username);
+        int del = accountRepo.deleteAccountById(acct.getId());
+        return del > 0;
+    }
+}
