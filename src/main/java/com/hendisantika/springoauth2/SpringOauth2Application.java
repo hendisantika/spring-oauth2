244e32827e9d1a0c33dc63e7fbab3cd197c10932
Hendi Santika <hendisantika@gmail.com>
2018-07-04 07:10:05 +0700

    #2 - Add project

---
+++ src/main/java/com/hendisantika/springoauth2/SpringOauth2Application.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauth2Application.class, args);
+    }
+}
