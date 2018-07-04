97a99957d34c207b80d58ec8dc9f075061421218
Hendi Santika <hendisantika@gmail.com>
2018-07-04 07:12:52 +0700

    #4 - Add Role Enum

---
+++ src/main/java/com/hendisantika/springoauth2/entity/Role.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springoauth2.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-oauth2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/07/18
+ * Time: 07.12
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN,
+    ROLE_REGISTER,
+    ROLE_TRUSTED_CLIENT,
+    ROLE_CLIENT
+}
