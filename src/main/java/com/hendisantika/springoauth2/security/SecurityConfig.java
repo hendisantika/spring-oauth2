7f127080d49489a7b815bf5999df9c78c4bceeeb
Hendi Santika <hendisantika@gmail.com>
2018-07-06 20:47:01 +0700

    #15 - Add config in application.properties

---
+++ src/main/java/com/hendisantika/springoauth2/security/SecurityConfig.java
@@ -0,0 +1,70 @@
+package com.hendisantika.springoauth2.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-oauth2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/07/18
+ * Time: 06.42
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebSecurity(debug = true)
+//@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+@Order(SecurityProperties.)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public UserDetailsService userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(userDetailsService());
+        return provider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .formLogin().disable() // disable form authentication
+                .anonymous().disable() // disable anonymous user
+                .httpBasic().and()
+                // restricting access to authenticated users
+                .authorizeRequests().anyRequest().authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        // provides the default AuthenticationManager as a Bean
+        return super.authenticationManagerBean();
+    }
+}
